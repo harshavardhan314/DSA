93c31b6fd5c10867d23f06fcf754fcc5a5942358
Harsha_314 <hharshavardhan974@gmail.com>
2025-12-01 21:52:41 +0530

    Time: 287 ms (17.95%) | Memory: 25.5 MB (21.94%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -1,19 +1,25 @@
-def check(curr):
-    return curr==curr[::-1]
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        n=len(s)
-        cnt=0
-        for i in range(n):
-            curr=""
-            for j in range(i,n):
-                curr+=s[j]
-                if check(curr):
-                    cnt+=1
-        
+        n = len(s)
+        cnt = 0
+        dp = [[False] * n for _ in range(n)]
+
+        for l in range(1, n + 1):          
+            for i in range(n):
+                j = i + l - 1             
+                if j >= n:
+                    break
+
+                if l == 1:                # single character
+                    dp[i][j] = True
+
+                elif l == 2:              # two characters
+                    dp[i][j] = (s[i] == s[j])
+
+                else:                     # longer substrings
+                    dp[i][j] = (s[i] == s[j]) and dp[i+1][j-1]
+
+                if dp[i][j]:
+                    cnt += 1
+
         return cnt
-
-
-
-
-        
