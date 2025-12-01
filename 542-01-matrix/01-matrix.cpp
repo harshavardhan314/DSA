b9a9dba5ac943786649df82996114de21f86e738
Harsha_314 <hharshavardhan974@gmail.com>
2025-12-01 11:04:55 +0530

    Time: 34 ms (17.51%) | Memory: 38.9 MB (21.82%) - LeetSync

---
+++ 542-01-matrix/01-matrix.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n = mat.size(), m = mat[0].size();
+
+        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+
+        priority_queue<
+            pair<int,pair<int,int>>,
+            vector<pair<int,pair<int,int>>>,
+            greater<pair<int,pair<int,int>>>
+        > pq;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(mat[i][j] == 0){
+                    dist[i][j] = 0;
+                    pq.push({0,{i,j}});
+                }
+            }
+        }
+
+        vector<pair<int,int>> dir = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        while(!pq.empty()){
+            auto top = pq.top();
+            pq.pop();
+
+            int d = top.first;
+            int x = top.second.first;
+            int y = top.second.second;
+
+            if(vis[x][y]) continue;
+            vis[x][y] = 1;
+
+            for(auto &p : dir){
+                int nx = x + p.first;
+                int ny = y + p.second;
+
+                if(nx>=0 && nx<n && ny>=0 && ny<m){
+                    if(dist[nx][ny] > d + 1){
+                        dist[nx][ny] = d + 1;
+                        pq.push({dist[nx][ny], {nx,ny}});
+                    }
+                }
+            }
+        }
+
+        return dist;
+    }
+};
