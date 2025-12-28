8076519cd1842b3f0d9f1b7e204ff86091bb6ca2
Harsha_314 <hharshavardhan974@gmail.com>
2025-12-28 23:59:33 +0530

    Time: 107 ms (92.02%) | Memory: 19.1 MB (81.65%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.py
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.py
@@ -15,12 +15,15 @@ class Solution:
         #     dp[idx1][idx2]=not_match
         #     return max(rec(idx1-1,idx2),rec(idx1,idx2-1))
         # longest=rec(n-1,m-1)
+        prev=[0]*(m+1)
+        curr=[0]*(m+1)
         for i in range(1,n+1):
             for j in range(1,m+1):
                 if s[i-1]==t[j-1]:
-                    dp[i][j]=1+dp[i-1][j-1]
+                    curr[j]=1+prev[j-1]
                 else:
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1])
-        longest = dp[n][m]
+                    curr[j]=max(prev[j],curr[j-1])
+            prev=list(curr)
+        longest = curr[m]
         return n-longest+m-longest
         
