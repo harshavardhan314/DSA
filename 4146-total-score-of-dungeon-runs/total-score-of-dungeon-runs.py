3f1a066dde1b15987bb540b65113278bea3b3a9e
Harsha_314 <hharshavardhan974@gmail.com>
2025-12-30 22:12:09 +0530

    Time: 375 ms (49.56%) | Memory: 37.5 MB (29.10%) - LeetSync

---
+++ 4146-total-score-of-dungeon-runs/total-score-of-dungeon-runs.py
@@ -0,0 +1,18 @@
+from collections import *
+class Solution:
+    def totalScore(self, hp: int, d: List[int], r: List[int]) -> int:
+        n=len(d)
+        pre=[0]*(n+1)
+        for i in range(1,n+1):
+            pre[i]=pre[i-1]+d[i-1]
+        ans=0
+        for i in range(1,n+1):
+            want=r[i-1]-hp+pre[i]
+            idx=bisect_left(pre,want,0,i+1)
+            idx=min(idx,i)
+            ans+=i-idx
+        return ans
+
+
+
+        
