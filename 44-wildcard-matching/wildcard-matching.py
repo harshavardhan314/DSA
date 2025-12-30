6b59f9947fcb6f2995a16d4e394f5f7ec906a4cd
Harsha_314 <hharshavardhan974@gmail.com>
2025-12-30 23:23:42 +0530

    Time: 872 ms (36.09%) | Memory: 48.7 MB (63.37%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.py
+++ 44-wildcard-matching/wildcard-matching.py
@@ -1,30 +1,51 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         n,m=len(s),len(p)
-        dp=[[-1]*m for _ in range(n)]
-        def rec(i,j):
-            if i==n and j==m:
-                return True
-            if j==m:
-                return False
-            if i==n:
-                for k in range(j,m):
-                    if p[k]!='*':
-                        return False
-                return True
-            if dp[i][j]!=-1:
-                return dp[i][j]
-            if p[j]=='?':
-                val=rec(i+1,j+1)
-                dp[i][j]=val
-                return dp[i][j]
-            if p[j]=='*':
-                dp[i][j]=rec(i,j+1) or rec(i+1,j)
-                return dp[i][j]
-            if s[i]==p[j]:
-                dp[i][j]=rec(i+1,j+1)
-                return dp[i][j]
-            return False
-        return rec(0,0)
+        dp=[[-1]*(m+1) for _ in range(n+1)]
+        # def rec(i,j):
+        #     if i==n and j==m:
+        #         return True
+        #     if j==m:
+        #         return False
+        #     if i==n:
+        #         for k in range(j,m):
+        #             if p[k]!='*':
+        #                 return False
+        #         return True
+        #     if dp[i][j]!=-1:
+        #         return dp[i][j]
+        #     if p[j]=='?':
+        #         val=rec(i+1,j+1)
+        #         dp[i][j]=val
+        #         return dp[i][j]
+        #     if p[j]=='*':
+        #         dp[i][j]=rec(i,j+1) or rec(i+1,j)
+        #         return dp[i][j]
+        #     if s[i]==p[j]:
+        #         dp[i][j]=rec(i+1,j+1)
+        #         return dp[i][j]
+        #     return False
+        found =True
+        for j  in range(1,m+1):
+            if p[j-1]!='*':
+                found=False
+            dp[0][j]=found
+        for i in range(n+1):
+            dp[i][0]=False
+        dp[0][0]=True
+        
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if p[j-1]=='?':
+                    dp[i][j]=dp[i-1][j-1]
+                elif p[j-1]=='*':
+                    dp[i][j]=dp[i-1][j] or dp[i][j-1]
+                elif s[i-1]==p[j-1]:
+                    dp[i][j]=dp[i-1][j-1]
+                else:
+                    dp[i][j]=False
+
+
+        return dp[n][m]
 
         
