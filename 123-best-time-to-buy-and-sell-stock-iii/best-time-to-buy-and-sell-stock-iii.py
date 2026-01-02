5ac4b822859d93c8f7f189cf87dbde2f7f2e68dc
Harsha_314 <hharshavardhan974@gmail.com>
2026-01-02 22:29:37 +0530

    Time: 2030 ms (5.91%) | Memory: 84.6 MB (14.07%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -1,19 +1,18 @@
 class Solution:
     def maxProfit(self, price: List[int]) -> int:
         n=len(price)
-        dp={}
+        dp=[[[-1]*3 for _ in range(2)] for _ in range(n)]
         def rec(i,buy,cnt):
             if i>=n or  cnt>2:
                 return 0
-            temp=str(buy)+"_"+str(cnt)
-            if (i,temp) in dp:
-                return dp[(i,temp)]
+            if  dp[i][buy][cnt]!=-1:
+                return dp[i][buy][cnt]
             if not buy:
                 profit=max( -price[i]+rec(i+1,1,cnt+1) , rec(i+1,0,cnt))
             else:
                 profit=max(price[i]+rec(i+1,0,cnt), rec(i+1,buy,cnt))
             
-            dp[(i,temp)]=profit
+            dp[i][buy][cnt]=profit
             return profit
         
         return rec(0,0,0)
