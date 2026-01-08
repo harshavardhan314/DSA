f956343ed25a0c81898d530ffd65337946492f73
Harsha_314 <hharshavardhan974@gmail.com>
2026-01-08 19:23:11 +0530

    Time: 127 ms (94.98%) | Memory: 21.9 MB (30.15%) - LeetSync

--- 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.py
+++ 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.py
@@ -1,19 +1,29 @@
 class Solution:
     def maxDotProduct(self, nums1: List[int], nums2: List[int]) -> int:
         n,m=len(nums1),len(nums2)
-        dp=[[-1]*m for _ in range(n)]
-        def rec(i,j):
-            if i==n or j==m:
-                return -int(1e9)
-            if dp[i][j]!=-1:
-                return dp[i][j]
-            take=nums1[i]*nums2[j]
-            skip_j=rec(i,j+1)
-            skip_i=rec(i+1,j)
-            ans=max(take+rec(i+1,j+1),take,skip_j,skip_i)
-            dp[i][j]=ans
-            return ans
-        return rec(0,0)
+        dp=[[0]*(m+1) for _ in range(n+1)]
+        # def rec(i,j):
+        #     if i==n or j==m:
+        #         return -int(1e9)
+        #     if dp[i][j]!=-1:
+        #         return dp[i][j]
+        #     take=nums1[i]*nums2[j]
+        #     skip_j=rec(i,j+1)
+        #     skip_i=rec(i+1,j)
+        #     ans=max(take+rec(i+1,j+1),take,skip_j,skip_i)
+        #     dp[i][j]=ans
+        #     return ans
+        for i in range(m+1):
+            dp[0][i]=-int(1e9)
+        for i in range(n+1):
+            dp[i][0]=-int(1e9)
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                take=nums1[i-1]*nums2[j-1]
+                skip_j=dp[i][j-1]
+                skip_i=dp[i-1][j]
+                dp[i][j]=max(take,take+dp[i-1][j-1],skip_i,skip_j)
+        return dp[n][m]
             
 
         
