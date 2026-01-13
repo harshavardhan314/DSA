dc93a3e8b485c26d79c0f48079f51772eab4db39
Harsha_314 <hharshavardhan974@gmail.com>
2026-01-13 11:32:55 +0530

    Time: 95 ms (66.76%) | Memory: 198.6 MB (73.99%) - LeetSync

--- 3763-separate-squares-i/separate-squares-i.cpp
+++ 3763-separate-squares-i/separate-squares-i.cpp
@@ -1,49 +1,54 @@
 class Solution {
 public:
-    bool check(double mid, vector<vector<int>>& s, double area) {
-        double low = 0, high = 0;
-        int n = s.size();
-
-        for (int i = 0; i < n; i++) {
-            double y = s[i][1];
-            double side = s[i][2];
-
-            if (y + side <= mid) {
-                low += side * side;
+    bool check(double mid,vector<vector<int>>&s,double area){
+        double low=0,high=0;
+        int n=s.size();
+        for(int i=0;i<n;i++)
+        {
+            double x=s[i][0];
+            double y=s[i][1];
+            double l=s[i][2];
+            if(y+l<=mid)
+            {
+                low+=(l*l*1.0);
             }
-            else if (y >= mid) {
-                high += side * side;
+            else if(y>=mid)
+            {
+                high+=(l*l*1.0);
             }
-            else {
-                double lower_part = (mid - y) * side;
-                low += lower_part;
-                high += side * side - lower_part;
-            }
-        }
-
-        return low >= area / 2.0;
-    }
-
-    double separateSquares(vector<vector<int>>& squares) {
-        double low = 0, high = 2e9;
-        double area = 0;
-
-        for (auto &sq : squares) {
-            double y = sq[1];
-            double side = sq[2];
-            area += side * side;
-        }
-
-        double eps = 1e-6;
-
-        while (high - low > eps) {
-            double mid = (low + high) / 2.0;
-            if (check(mid, squares, area))
-                high = mid;
             else
-                low = mid;
-        }
+            {
+                double total=(l*l);
+                double lower_part=(mid-y)*l;
+                low+=lower_part;
+                high+=total-lower_part;
+            }
 
-        return low;
+        }
+        return low>=(area)/2.0;
+    }
+    double separateSquares(vector<vector<int>>& squares) {
+        double l=0;
+        double h=2e9;
+        double ans=0;
+        double area=0;
+        for(int i=0;i<squares.size();i++)
+        {
+            double side=squares[i][2];
+            area+=side*side;
+
+        }
+        while((h-l)>1e-5)
+        {
+            double mid=(l+h)/2;
+            if(check(mid,squares,area))
+            {
+                h=mid;
+            }
+            else
+            l=mid;
+        }
+        return l;
+        
     }
 };
