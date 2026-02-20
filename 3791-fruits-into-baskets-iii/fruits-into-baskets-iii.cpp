4d90b03eb40e3c4011fbebc0423e9feff19e381d
Harsha_314 <hharshavardhan974@gmail.com>
2026-02-20 13:00:40 +0530

    Time: 1710 ms (5.03%) | Memory: 218.6 MB (5.03%) - LeetSync

--- 3791-fruits-into-baskets-iii/fruits-into-baskets-iii.cpp
+++ 3791-fruits-into-baskets-iii/fruits-into-baskets-iii.cpp
@@ -1,73 +1,67 @@
-class SegTree {
+class segmentTree {
 public:
+    vector<long long> seg;
     int n;
-    vector<int> seg;
-    SegTree(vector<int>& arr) {
-        n = arr.size();
-        seg.resize(4 * n);
-        build(0, 0, n - 1, arr);
+
+    segmentTree(int _n) {
+        this->n = _n;
+        seg.resize(4 * n + 1, 0);
     }
 
-    void build(int idx, int low, int high, vector<int>& arr) {
+    void build(const vector<int>& arr, int idx, int low, int high) {
         if (low == high) {
             seg[idx] = arr[low];
             return;
         }
         int mid = (low + high) / 2;
-        build(2*idx+1, low, mid, arr);
-        build(2*idx+2, mid+1, high, arr);
-        seg[idx] = max(seg[2*idx+1], seg[2*idx+2]);
+        build(arr, 2 * idx + 1, low, mid);
+        build(arr, 2 * idx + 2, mid + 1, high);
+        seg[idx] = max(seg[2 * idx + 1], seg[2 * idx + 2]);
     }
 
-    int query(int idx, int low, int high, int l, int r) {
-        if (r < low || high < l) return 0;
-        if (l <= low && high <= r) return seg[idx];
-        int mid = (low + high) / 2;
-        return max(
-            query(2*idx+1, low, mid, l, r),
-            query(2*idx+2, mid+1, high, l, r)
-        );
-    }
-
-    void update(int idx, int low, int high, int pos, int val) {
+    void update(int idx, int low, int high, int target) {
         if (low == high) {
-            seg[idx] = val;
+            seg[idx] = 0;
             return;
         }
         int mid = (low + high) / 2;
-        if (pos <= mid)
-            update(2*idx+1, low, mid, pos, val);
-        else
-            update(2*idx+2, mid+1, high, pos, val);
-        seg[idx] = max(seg[2*idx+1], seg[2*idx+2]);
+        if (target <= mid) update(2 * idx + 1, low, mid, target);
+        else update(2 * idx + 2, mid + 1, high, target);
+        seg[idx] = max(seg[2 * idx + 1], seg[2 * idx + 2]);
+    }
+
+    int findLeftmost(int idx, int low, int high, int val) {
+        if (seg[idx] < val) return -1; 
+        if (low == high) return low;   
+
+        int mid = (low + high) / 2;
+        
+        if (seg[2 * idx + 1] >= val) {
+            return findLeftmost(2 * idx + 1, low, mid, val);
+        } else {
+            return findLeftmost(2 * idx + 2, mid + 1, high, val);
+        }
     }
 };
 class Solution {
 public:
     int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
         int n = fruits.size();
-        SegTree seg(baskets);
+
+        segmentTree st(n);
+        st.build(baskets,0,0,n-1);
 
         int cnt = 0;
 
         for (int i = 0; i < n; i++) {
-            int l = 0, h = n - 1;
-            int ans = -1;
 
-            while (l <= h) {
-                int mid = (l + h) / 2;
-                if (seg.query(0, 0, n - 1, 0, mid) >= fruits[i]) {
-                    ans = mid;
-                    h = mid - 1;
-                } else {
-                    l = mid + 1;
-                }
-            }
+            int ans=st.findLeftmost(0,0,n-1,fruits[i]);
 
+            cout<<ans<<endl;
             if (ans == -1) {
                 cnt++; 
             } else {
-                seg.update(0, 0, n - 1, ans, 0); 
+                st.update(0, 0, n - 1, ans); 
             }
         }
         return cnt;
