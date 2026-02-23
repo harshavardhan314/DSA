f6133821f2602045169400fa4e1bb14791ce89f8
Harsha_314 <hharshavardhan974@gmail.com>
2026-02-23 11:56:22 +0530

    Time: 1 ms (45.42%) | Memory: 14.2 MB (99.19%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -5,7 +5,6 @@ public:
     int minFallingPathSum(vector<vector<int>>& dp) {
 
         int n=dp.size();
-        int ans=INT_MAX;
         for(int i=1;i<n;i++){
             for(int j=0;j<n;j++){
                 dp[i][j]=dp[i][j]+min({dp[i-1][j] ,dp[i-1][max(0,j-1)],dp[i-1][min(n-1,j+1)]});
