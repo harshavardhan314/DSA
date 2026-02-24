d8376e74adaf33a2a87257fc30cdb75d346783af
Harsha_314 <hharshavardhan974@gmail.com>
2026-02-24 12:59:45 +0530

    Time: 85 ms (34.74%) | Memory: 242.9 MB (37.66%) - LeetSync

--- 1294-range-frequency-queries/range-frequency-queries.cpp
+++ 1294-range-frequency-queries/range-frequency-queries.cpp
@@ -1,63 +1,21 @@
-class segmentTree{
-    public:
-    vector<unordered_map<int,int>>seg;
-    int n;
-    segmentTree(){}
-    segmentTree(int _n){
-        n=_n;
-        seg.resize(4*n);
-    }
-
-    void build(vector<int>&nums,int idx,int low,int high){
-        if(low==high){
-            seg[idx][nums[low]]++;
-            return;
-        }
-        int mid=(low+high)/2;
-        build(nums,2*idx+1,low,mid);
-        build(nums,2*idx+2,mid+1,high);
-
-        for(auto [key,val]:seg[2*idx+1]){
-            seg[idx][key]+=val;
-        }
-        for(auto [key,val]:seg[2*idx+2]){
-            seg[idx][key]+=val;
-        }
-
-    }
-    int query(int idx,int low,int high,int l, int h,int val){
-        if(low>=l && high<=h){
-            if (seg[idx].find(val)!=seg[idx].end())
-            return seg[idx][val];
-            return 0;
-        }
-        if(l>high || h<low){
-            return 0;
-        }
-        int mid=(low+high)/2;
-        return query(2*idx+1,low,mid,l,h,val) + query(2*idx+2,mid+1,high,l,h,val);
-
-
-    }
-};
-
-
 class RangeFreqQuery {
 public:
-    int n;
-    segmentTree st;
+    map<int,vector<int>>mp;
     RangeFreqQuery(vector<int>& arr) {
-        n=arr.size();
-        st =segmentTree(n);
-        st.build(arr,0,0,n-1);
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            mp[arr[i]].push_back(i);
+        }
     }
     
     int query(int left, int right, int val) {
-        return st.query(0,0,n-1,left,right,val);
-        
+
+        int r_pos=upper_bound(mp[val].begin(),mp[val].end(),right)-mp[val].begin();
+        int l_pos=lower_bound(mp[val].begin(),mp[val].end(),left)-mp[val].begin();
+        return r_pos-l_pos;
     }
 };
-auto init = atexit([]() { ofstream("display_runtime.txt") << "0"; });
+
 /**
  * Your RangeFreqQuery object will be instantiated and called as such:
  * RangeFreqQuery* obj = new RangeFreqQuery(arr);
