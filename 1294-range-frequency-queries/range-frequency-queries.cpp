c5854c708ab68256291f140a53ce51668a9ea360
Harsha_314 <hharshavardhan974@gmail.com>
2026-02-24 12:45:35 +0530

    Time: 647 ms (8.77%) | Memory: 588.6 MB (21.27%) - LeetSync

---
+++ 1294-range-frequency-queries/range-frequency-queries.cpp
@@ -0,0 +1,65 @@
+class segmentTree{
+    public:
+    vector<unordered_map<int,int>>seg;
+    int n;
+    segmentTree(){}
+    segmentTree(int _n){
+        n=_n;
+        seg.resize(4*n);
+    }
+
+    void build(vector<int>&nums,int idx,int low,int high){
+        if(low==high){
+            seg[idx][nums[low]]++;
+            return;
+        }
+        int mid=(low+high)/2;
+        build(nums,2*idx+1,low,mid);
+        build(nums,2*idx+2,mid+1,high);
+
+        for(auto [key,val]:seg[2*idx+1]){
+            seg[idx][key]+=val;
+        }
+        for(auto [key,val]:seg[2*idx+2]){
+            seg[idx][key]+=val;
+        }
+
+    }
+    int query(int idx,int low,int high,int l, int h,int val){
+        if(low>=l && high<=h){
+            if (seg[idx].find(val)!=seg[idx].end())
+            return seg[idx][val];
+            return 0;
+        }
+        if(l>high || h<low){
+            return 0;
+        }
+        int mid=(low+high)/2;
+        return query(2*idx+1,low,mid,l,h,val) + query(2*idx+2,mid+1,high,l,h,val);
+
+
+    }
+};
+
+
+class RangeFreqQuery {
+public:
+    int n;
+    segmentTree st;
+    RangeFreqQuery(vector<int>& arr) {
+        n=arr.size();
+        st =segmentTree(n);
+        st.build(arr,0,0,n-1);
+    }
+    
+    int query(int left, int right, int val) {
+        return st.query(0,0,n-1,left,right,val);
+        
+    }
+};
+
+/**
+ * Your RangeFreqQuery object will be instantiated and called as such:
+ * RangeFreqQuery* obj = new RangeFreqQuery(arr);
+ * int param_1 = obj->query(left,right,value);
+ */
