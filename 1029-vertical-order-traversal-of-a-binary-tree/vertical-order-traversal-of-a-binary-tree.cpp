03b24f4b4f20636597da11323f8ccb80bd67e1f1
Harsha_314 <hharshavardhan974@gmail.com>
2026-03-04 21:40:59 +0530

    Time: 3 ms (39.46%) | Memory: 16.2 MB (51.70%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -13,7 +13,7 @@ vector<vector<int>> level_order(TreeNode*root){
 
     vector<pair<int,int>>temp;
     queue<pair<int,TreeNode*>>q;
-    map<int,vector<pair<int,int>>>mp;
+    map<int,multiset<pair<int,int>>>mp;
     q.push({0,root});
     int level=0;
     while(q.size()>0){
@@ -25,7 +25,7 @@ vector<vector<int>> level_order(TreeNode*root){
             if(node==NULL){
                 continue;
             }
-            mp[col].push_back({level,node->val});
+            mp[col].insert({level,node->val});
             q.push({col-1,node->left});
             q.push({col+1,node->right});
             
@@ -35,11 +35,10 @@ vector<vector<int>> level_order(TreeNode*root){
     vector<vector<int>>res;
     for(auto it:mp){
         
-        vector<pair<int,int>>vp=it.second;
-        vector<int>t;
-        sort(vp.begin(),vp.end());
         
-        for(auto itr:vp){
+        vector<int>t;
+        multiset<pair<int,int>>values=it.second;
+        for(auto itr:values){
             t.push_back(itr.second);
         }
         
