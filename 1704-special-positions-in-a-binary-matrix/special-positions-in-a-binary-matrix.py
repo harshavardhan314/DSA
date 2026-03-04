bfb25cf77589e101878ba1ebe5f6c3f6dcf0094e
Harsha_314 <hharshavardhan974@gmail.com>
2026-03-04 18:24:25 +0530

    Time: 3 ms (88.03%) | Memory: 19.6 MB (93.85%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.py
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.py
@@ -4,20 +4,19 @@ class Solution:
         m=len(mat[0])
         ans=0
 
+        row=[0]*n
+        col=[0]*m
+
         for i in range(n):
             for j in range(m):
                 if mat[i][j]==1:
-                    valid=True
-                    for k in range(m):
-                        if k!=j and mat[i][k]==1:
-                            valid=False
-                    
-                    for k in range(n):
-                        if k!=i and mat[k][j]==1:
-                            valid=False
-                    
-                    if valid:
-                        ans+=1
+                    row[i]+=1
+                    col[j]+=1
         
+
+        for i in range(n):
+            for j in range(m):
+                if mat[i][j]==1 and row[i]==1 and col[j]==1:
+                    ans+=1        
         return ans
                             
