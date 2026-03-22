08de6599d77b4ee3c330e674dcc36eadcd38a284
Harsha_314 <hharshavardhan974@gmail.com>
2026-03-22 20:07:13 +0530

    Time: 467 ms (16.31%) | Memory: 145.6 MB (38.41%) - LeetSync

--- 4154-maximum-bitwise-and-after-increment-operations/maximum-bitwise-and-after-increment-operations.cpp
+++ 4154-maximum-bitwise-and-after-increment-operations/maximum-bitwise-and-after-increment-operations.cpp
@@ -1,19 +1,34 @@
-int get(int x,int t){
+    // to get the y>=x we go from msb to lsb where we have :
+    // x   y
+    // 1   0---> keep as same as x
+    // 1   1---> keep as same as x
+    // 0   1---> this is the point where y > x and keep the remaining bits same ...
+    // 0   0---> keep same
+
+int get_ops(int x,int target){
     int y=0;
     bool match=true;
-    for(int i=30;i>=0;i--){
-
-        if(match && (t&(1LL<<i)) && !(x&(1LL<<i)))match=false;
-
-        if(match)y|=x&(1LL<<i);
-        else y|=t&(1LL<<i);
+    for(int i=31;i>=0;i--){
+        int curr_bit=1LL<<i;
+        if(!(curr_bit&x) && (target&curr_bit))
+        {
+            match=false;
+        }
+        if(match){
+            if(x&curr_bit)
+            y|=curr_bit;
+        }
+        else{
+            if(target&curr_bit)y|=curr_bit;
+        }
     }
     return y-x;
-}
 
+}
 class Solution {
 public:
     int maximumAND(vector<int>& nums, int k, int m) {
+
         int n=nums.size();
         int mask=0;
 
@@ -21,7 +36,7 @@ public:
 
             vector<int>cost;
             for(int j=0;j<nums.size();j++){
-                cost.push_back(get(nums[j],mask|(1LL<<i)));
+                cost.push_back(get_ops(nums[j],mask|(1LL<<i)));
             }
 
             sort(cost.begin(),cost.end());
@@ -33,7 +48,5 @@ public:
             if(sum<=k)mask|=(1LL<<i);
         }
         return mask;
-        
-        
     }
 };
