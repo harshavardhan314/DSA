af6b63a57a96fff095e9a275b6d3515c0078338a
Harsha_314 <hharshavardhan974@gmail.com>
2026-05-01 18:45:53 +0530

    Time: 895 ms (40.32%) | Memory: 499.4 MB (63.53%) - LeetSync

--- 3986-maximum-path-score-in-a-grid/maximum-path-score-in-a-grid.cpp
+++ 3986-maximum-path-score-in-a-grid/maximum-path-score-in-a-grid.cpp
@@ -1,36 +1,44 @@
 class Solution {
 public:
-    int rec(int i, int j, int cost, int k,
-            vector<vector<int>>& grid,
-            vector<vector<vector<int>>>& dp) {
+   
 
-        int n = grid.size(), m = grid[0].size();
-        if (i == n || j == m) return -1;
+    long long solve(int i, int j, vector<vector<int>>& grid, int cost, int k,vector<vector<vector<int>>>&dp) {
+        int n = grid.size();
+        int m = grid[0].size();
 
-        cost += (grid[i][j] > 0);
-        if (cost > k) return -1;
+        if (cost > k) return -1e9;
 
-        if (i == n-1 && j == m-1)
-            return dp[i][j][cost] = grid[i][j];
+        if (i == n-1 && j == m-1) {
+            int newCost = cost + (grid[i][j] > 0);
+            if (newCost > k) return -1e9;
+            return grid[i][j];
+        }
 
-        if (dp[i][j][cost] != INT_MIN)
-            return dp[i][j][cost];
+        if (dp[i][j][cost] != -1e9) return dp[i][j][cost];
 
-        int right = rec(i, j+1, cost, k, grid, dp);
-        int down  = rec(i+1, j, cost, k, grid, dp);
+        int newCost = cost + (grid[i][j] > 0);
 
-        int best = max(right, down);
-        if (best >= 0)
-            return dp[i][j][cost] = best + grid[i][j];
-        else
-            return dp[i][j][cost] = -1;  
+        long long right = -1e9, down = -1e9;
+
+        if (j + 1 < m)
+            right = grid[i][j] + solve(i, j+1, grid, newCost, k,dp);
+
+        if (i + 1 < n)
+            down = grid[i][j] + solve(i+1, j, grid, newCost, k,dp);
+
+        return dp[i][j][cost] = max(right, down);
     }
 
     int maxPathScore(vector<vector<int>>& grid, int k) {
-        int n = grid.size(), m = grid[0].size();
-        vector<vector<vector<int>>> dp(
-            n, vector<vector<int>>(m, vector<int>(k+1, INT_MIN))
-        );
-        return rec(0, 0, 0, k, grid, dp);
+
+        int n=grid.size();
+        int m=grid[0].size();
+
+        vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(k+1,-1e9)));
+
+        long long res = solve(0, 0, grid, 0, k,dp);
+
+        if (res < 0) return -1;
+        return (int)res;
     }
 };
