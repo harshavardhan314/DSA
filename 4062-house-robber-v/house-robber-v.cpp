2c6359b544dbdb39eb18d6250e94551dac7359bb
Harsha_314 <hharshavardhan974@gmail.com>
2026-05-02 17:34:27 +0530

    Time: 193 ms (11.89%) | Memory: 302.3 MB (12.30%) - LeetSync

--- 4062-house-robber-v/house-robber-v.cpp
+++ 4062-house-robber-v/house-robber-v.cpp
@@ -1,23 +1,39 @@
-long long rec(int idx,vector<int>&nums,vector<int>&colors, vector<long long> &dp){
-    if(idx>=nums.size())return 0;
-    if(dp[idx]!=-1) return dp[idx];
-    long long take=nums[idx],not_take=0;
-    not_take=rec(idx+1,nums,colors,dp);
-    if(idx+1<nums.size())
-    {
-        if(colors[idx]!=colors[idx+1])
-        take+=rec(idx+1,nums,colors,dp);
-        else
-        take+=rec(idx+2,nums,colors,dp);
+long long int  solve( int  i,vector< int >&nums,vector< int >&c,vector<long long int >&dp){
+    long long int  n=nums.size();
+    if(i>=n)return 0;
+    if(dp[i]!=-1)return dp[i];
+    long long int  pick=nums[i]+solve(i+2,nums,c,dp);
+    long long int  next_cost=0;
+    if(i+1<n && c[i]!=c[i+1]){
+        next_cost=nums[i]+solve(i+1,nums,c,dp);
     }
-   
-    return dp[idx]=max(take,not_take);
+    long long int  not_pick=solve(i+1,nums,c,dp);
+    return dp[i]=max({pick,not_pick,next_cost});
 }
+
+
+
+
 class Solution {
 public:
-    long long rob(vector<int>& nums, vector<int>& colors) {
-        vector<long long> dp(nums.size()+2,-1);
-        return rec(0,nums,colors,dp);
-        
+    long long rob(vector<int >& nums, vector<int >& c) {
+        int  n=nums.size();
+        vector<vector<long long >>dp(n,vector<long long >(2,0));
+        dp[0][1]=nums[0];
+
+        for(int i=1;i<n;i++){
+            dp[i][0]=max(dp[i-1][0],dp[i-1][1]);
+
+            dp[i][1]=nums[i];
+
+            if(c[i-1]!=c[i])
+            dp[i][1]=max(dp[i][1],nums[i]+max(dp[i-1][0],dp[i-1][1]));
+            
+            if(i-2>=0)
+            dp[i][1]=max(dp[i][1],nums[i]+max(dp[i-2][0],dp[i-2][1]));
+        }
+        return max(dp[n-1][0],dp[n-1][1]);
+
+
     }
 };
