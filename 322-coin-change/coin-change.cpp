36a509639ed67cf4cd35b6dff1ff64428b525541
Harsha_314 <hharshavardhan974@gmail.com>
2026-05-03 20:37:54 +0530

    Time: 62 ms (11.33%) | Memory: 49.7 MB (21.88%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,30 +1,32 @@
+int rec(int i,vector<int>&coins,int a,vector<vector<int>>&dp){
+
+    int n=coins.size();
+
+    if(i>=n){
+        if(a==0)return 0;
+        return 1e9;
+    }
+    if(dp[i][a]!=-1)return dp[i][a];
+
+    int pick=1e9;
+    if(coins[i]<=a){
+        pick=1+rec(i,coins,a-coins[i],dp);
+    }
+    int not_pick=rec(i+1,coins,a,dp);
+    return dp[i][a]=min(pick,not_pick);
+
+}
+
 class Solution {
 public:
-    int rec(int i, int amount,vector<int>&arr,vector<vector<int>>&dp){
-        if(amount == 0 )
-        {
-            return 0;
+    int coinChange(vector<int>& coins, int a) {
 
-        }
-        if(i<0||amount<0)
-        {
-            return INT_MAX;
-        }
-        if(dp[i][amount]!=-1)return dp[i][amount];
-        int pick=rec(i,amount-arr[i],arr,dp);
-        if(pick!=INT_MAX)pick+=1;
-        int not_pick=rec(i-1,amount,arr,dp);
-
-        return dp[i][amount]=min(pick,not_pick);
-
-
-    }
-    int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
-        int ans= rec(n-1,amount,coins,dp);
+        vector<vector<int>>dp(n,vector<int>(a+1,-1));
 
-        return ans==INT_MAX?-1:ans;
+        int val=rec(0,coins,a,dp);
+        if(val==1e9)return -1;
+        return val;
         
     }
 };
