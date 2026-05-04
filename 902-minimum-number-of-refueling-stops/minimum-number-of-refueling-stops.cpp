ff8ea05dd9dee910b1664f0fbcfb32be78f73f1a
Harsha_314 <hharshavardhan974@gmail.com>
2026-05-04 09:34:59 +0530

    Time: 3 ms (35.17%) | Memory: 21.3 MB (9.43%) - LeetSync

--- 902-minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.cpp
+++ 902-minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
-    int minRefuelStops(int target, int s, vector<vector<int>>& st) {
-        int fuel=s;
-        int stops=0;
-        int n=st.size();
-        priority_queue<int>pq;
-        for (int i=0;i<n;i++){
-            int dist=st[i][0];
-            int f=st[i][1];
-            while(fuel<dist && !pq.empty()){
-                fuel+=pq.top();
-                pq.pop();
-                stops++;
-            }
-            if(fuel>=dist){
-                pq.push(f);
+    int minRefuelStops(int t, int sf, vector<vector<int>>& s) {
 
+
+        int val=sf;
+        int ans=0;
+
+        priority_queue<int>pq;
+        for(auto it:s){
+            int st_pt=it[0];
+            int dist=it[1];
+            while(!pq.empty() && val<st_pt){
+                val+=pq.top();
+                ans++;
+                pq.pop();
             }
-            else
-            {
+            if(val>=st_pt){
+                pq.push(dist);
+            }
+            else{
                 return -1;
             }
+
         }
-        while(fuel<target && !pq.empty()){
-            fuel+=pq.top();
+        while(!pq.empty() && val<t){
+            val+=pq.top();
             pq.pop();
-            stops++;
+            ans++;
+
         }
-        if(fuel<target)return -1;
-        return stops;
+        if(val<t) return -1;
+        return ans;
+
         
     }
 };
