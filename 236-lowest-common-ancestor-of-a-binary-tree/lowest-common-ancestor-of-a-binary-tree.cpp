ee383f5bb2b4215beaca3ef9bc44d7853e08c94c
Harsha_314 <hharshavardhan974@gmail.com>
2026-05-13 22:14:00 +0530

    Time: 11 ms (69.62%) | Memory: 18.7 MB (7.51%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -7,38 +7,46 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-bool find(TreeNode*root,vector<TreeNode*>&path,TreeNode* target){
-    if(!root)return false;
-    path.push_back(root);
-    if(root->val==target->val)return true;
-    
-    bool left=find(root->left,path,target);
-    bool right= find(root->right,path,target);
-    if(left || right)return true;
-    path.pop_back();
-    return false;
 
+bool rec(TreeNode* root, vector<TreeNode*>&path,TreeNode* req){
+
+    path.push_back(root);
+    if(root->val==req->val){
+        return true;
+    }
+    if(root->left){
+        bool left=rec(root->left,path,req);
+        if(left==true)return true;
+        else{
+            path.pop_back();
+        }
+    }
+    if(root->right){
+        bool right=rec(root->right,path,req);
+        if(right)return true;
+        else path.pop_back();
+    }
+    
+    return false;
 }
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
 
         vector<TreeNode*>path1,path2;
-        find(root,path1,p);
-        find(root,path2,q);
-        int i=0,j=0;
-        TreeNode* ans;
-        while(i<path1.size() && j<path2.size()){
-            if(path1[i]->val==path2[i]->val){
-                ans=path1[i];
-            }
-            else
-            break;
-            i++;
-            j++;
-        }
-        return ans;
 
+        rec(root,path1,p);
+        rec(root,path2,q);
+
+       int n=path1.size();
+       int m=path2.size();
+       TreeNode* ans=NULL;
+       for(int i=0;i<min(n,m);i++){
+        if(path1[i]->val==path2[i]->val)ans=path1[i];
+        
+       }
+
+        return ans;
         
     }
 };
