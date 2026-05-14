164fd1f11474c0a11366fd1a47434a6b8404dca0
Harsha_314 <hharshavardhan974@gmail.com>
2026-05-14 19:28:52 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (85.51%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,49 +1,46 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        queue<pair<int,int>>q;
 
         int n=grid.size();
         int m=grid[0].size();
-
         int fresh=0;
-
+        queue<pair<int,int>>q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
+
+                if(grid[i][j]==1)fresh++;
                 if(grid[i][j]==2){
                     q.push({i,j});
                 }
-                if(grid[i][j]==1)fresh++;
             }
         }
 
-        int min=0;
+        if(fresh==0)return 0;
 
-        vector<pair<int,int>>dir={{-1,0},{1,0},{0,-1},{0,1}};
+        vector<pair<int,int>>vp={{-1,0},{1,0},{0,1},{0,-1}};
+        int time=-1;
 
-        while(!q.empty() && fresh>0){
-            int sz=q.size();
-
-            for(int i=0;i<sz;i++){
-
-                auto[x,y]=q.front();
+        while(!q.empty() ){
+            int s=q.size();
+            time++;
+            for(int i=0;i<s;i++){
+                auto [x,y]=q.front();
                 q.pop();
-                
-
-                for(int j=0;j<4;j++){
-                    int ni=x+dir[j].first;
-                    int nj=y+dir[j].second;
+                for(auto [di,dj]:vp){
+                    int ni=x+di;
+                    int nj=y+dj;
                     if(ni>=0 && ni<n && nj>=0 && nj<m && grid[ni][nj]==1){
-                        fresh--;
-                        grid[ni][nj]=2;
                         q.push({ni,nj});
+                        grid[ni][nj]=2;
+                        fresh--;
                     }
                 }
             }
-            min++;
-        }
-        
-        if(fresh==0)return min;
-        return -1;
+            
+        }   
+        if(fresh>0)return -1;
+
+        return time;     
     }
 };
