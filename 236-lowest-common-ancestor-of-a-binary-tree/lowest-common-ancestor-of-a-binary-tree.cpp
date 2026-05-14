08168ce5fa860c7b314a1ed6f20e79af7bdcd8ce
Harsha_314 <hharshavardhan974@gmail.com>
2026-05-14 15:34:35 +0530

    Time: 10 ms (79.84%) | Memory: 17.2 MB (87.63%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -8,45 +8,22 @@
  * };
  */
 
-bool rec(TreeNode* root, vector<TreeNode*>&path,TreeNode* req){
+TreeNode* rec(TreeNode*root,TreeNode* p,TreeNode* q){
 
-    path.push_back(root);
-    if(root->val==req->val){
-        return true;
-    }
-    if(root->left){
-        bool left=rec(root->left,path,req);
-        if(left==true)return true;
-        else{
-            path.pop_back();
-        }
-    }
-    if(root->right){
-        bool right=rec(root->right,path,req);
-        if(right)return true;
-        else path.pop_back();
-    }
-    
-    return false;
+    if(root==NULL || root->val==p->val || root->val==q->val)return root;
+
+    TreeNode* left=rec(root->left,p,q);
+    TreeNode* right=rec(root->right,p,q);
+
+    if(left==NULL)return right;
+    else if(right==NULL)return left;
+    else return root;
 }
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
 
-        vector<TreeNode*>path1,path2;
-
-        rec(root,path1,p);
-        rec(root,path2,q);
-
-       int n=path1.size();
-       int m=path2.size();
-       TreeNode* ans=NULL;
-       for(int i=0;i<min(n,m);i++){
-        if(path1[i]->val==path2[i]->val)ans=path1[i];
-        
-       }
-
-        return ans;
+        return rec(root,p,q);
         
     }
 };
