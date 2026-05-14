ab696531914d1bcc379b105214f5108f682770e2
Harsha_314 <hharshavardhan974@gmail.com>
2026-05-14 23:54:00 +0530

    Time: 4 ms (63.69%) | Memory: 20.2 MB (17.43%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,31 +1,32 @@
-bool dfs(int node,vector<int>&vis,vector<int>&path_vis,vector<vector<int>>&adj){
-    vis[node]=1;
-    path_vis[node]=1;
-    for(auto it:adj[node]){
-        if(!vis[it]){
-            if(dfs(it,vis,path_vis,adj))return true;
-        }
-        else if(path_vis[it])return true;
-    }
-    path_vis[node]=0;
-    return false;
-}
-
 class Solution {
 public:
     bool canFinish(int n, vector<vector<int>>& p) {
         
+        vector<int>indeg(n,0);
+
         vector<vector<int>>adj(n);
         for(auto it:p){
             adj[it[1]].push_back(it[0]);
+            indeg[it[0]]++;
         }
-        vector<int>vis(n,0);
-        vector<int>path_vis(n,0);
+        queue<int>q;
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                if(dfs(i,vis,path_vis,adj))return false;
+            if(indeg[i]==0)
+            q.push(i);
+        
+        }
+        if(q.empty())return false;
+        
+        vector<int>order;
+        while(!q.empty()){
+            int node=q.front();
+            order.push_back(node);
+            q.pop();
+            for(auto it:adj[node]){
+                indeg[it]--;
+                if(indeg[it]==0)q.push(it);
             }
         }
-        return true;
+        return n==order.size();
     }
 };
