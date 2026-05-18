3ce800961d1eadf6f262aa9bc706e4f1b94b1bb1
Harsha_314 <hharshavardhan974@gmail.com>
2026-05-18 18:40:50 +0530

    Time: 3 ms (25.47%) | Memory: 69.2 MB (52.17%) - LeetSync

--- 2591-frog-jump-ii/frog-jump-ii.cpp
+++ 2591-frog-jump-ii/frog-jump-ii.cpp
@@ -1,36 +1,14 @@
-bool check(int mid,vector<int>&stones){
 
-    int ans=stones[1]-stones[0];
-    int n=stones.size();
-    for(int i=2;i<n;i++){
-        ans=max(ans,stones[i]-stones[i-2]);
-    }
-    return ans<=mid;
-    
-}
 
 class Solution {
 public:
     int maxJump(vector<int>& stones) {
 
-        int n=stones.size();
-        int l=0;
-        int h=stones.back()-stones[0]+1;
-        int ans=h;
-
-        if(n==2){
-            return stones[1]-stones[0];
+        int ans = stones[1] - stones[0];
+        int n = stones.size();
+        for (int i = 2; i < n; i++) {
+            ans = max(ans, stones[i] - stones[i - 2]);
         }
-        while(l<=h){
-            int mid=(l+h)/2;
-            if(check(mid,stones)){
-                ans=min(ans,mid);
-                h=mid-1;
-            }
-            else{
-                l=mid+1;
-            }
-        }
-        return ans;
+        return ans ;
     }
 };
