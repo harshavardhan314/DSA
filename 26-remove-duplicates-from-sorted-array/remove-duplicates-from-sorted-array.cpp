ecf55193f067cf6417b9080e8ae826ff3465aa2c
Harsha_314 <hharshavardhan974@gmail.com>
2026-05-24 17:37:15 +0530

    Time: 0 ms (100.00%) | Memory: 22.7 MB (18.07%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -4,22 +4,18 @@ public:
        
         vector<int>new_nums;
         int n=nums.size();
+        int j=0;
         for(int i=1;i<n;i++){
             if(nums[i]!=nums[i-1]){
-                new_nums.push_back(nums[i-1]);
+                nums[j]=nums[i-1];
+                j++;
             }
         }
-        new_nums.push_back(nums.back());
-        int j=0;
-        for(int i=0;i<new_nums.size();i++){
-            nums[i]=new_nums[j];
-            j++;
-        }
-        int ans=new_nums.size();
-        return ans;
+        nums[j]=nums.back();
 
-        // T.c=O(n)+O(k)
-        // S.c=O(k)
-        // where k is no of unique elements in the array..
+        return j+1;
+
+        // T.c=O(n)
+        // S.c=O(1)
     }
 };
