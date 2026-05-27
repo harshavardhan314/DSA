8033d7826f57e60853015b100871b57eca33bb83
Harsha_314 <hharshavardhan974@gmail.com>
2026-05-27 19:32:01 +0530

    Time: 20 ms (67.43%) | Memory: 17.5 MB (52.06%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -3,35 +3,46 @@ public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
 
 
-        // brute..
+        
         int n=nums.size();
-        set<vector<int>>st;
-        unordered_map<long long int,int>mp;
-        for(auto it:nums)mp[it]++;
+        sort(nums.begin(),nums.end());
+
+        // fix the first two points and then search for remaining two using two pointer approach..
+        vector<vector<int>>res;
         for(int i=0;i<n;i++){
+
+            if(i>0 && nums[i]==nums[i-1])continue;
+
             for(int j=i+1;j<n;j++){
-                for(int k=j+1;k<n;k++){
-                    
-                    mp[nums[i]]--;
-                    mp[nums[j]]--;
-                    mp[nums[k]]--;
 
-                        long long int sum=1LL*nums[i]+nums[j]+nums[k];
-                        long long rem=target-sum;
-                        if(mp.find(rem)!=mp.end() && mp[rem]>0){
-                            vector<int>temp{nums[i],nums[j],nums[k],(int)rem};
-                            sort(temp.begin(),temp.end());
-                            st.insert(temp);
-                        }
+                if(j>i+1 && nums[j]==nums[j-1])continue;
 
-                    mp[nums[i]]++;
-                    mp[nums[j]]++;
-                    mp[nums[k]]++;
-                    
+                int low=j+1;
+                int high=n-1;
+
+                while(low<high){
+                    long long sum=1LL * nums[i] + nums[j] + nums[low] + nums[high];
+
+                    if(sum<target){
+                        low++;
+                    }
+                    else if(sum>target){
+                        high--;
+                    }
+                    else{
+                        vector<int>temp{nums[i],nums[j],nums[low],nums[high]};
+                        res.push_back(temp);
+                        low++;
+                        high--;
+                        while(low<high && nums[low]==nums[low-1])low++;
+                        while(low<high && nums[high]==nums[high+1])high--;
+                    }
                 }
+
+
             }
         }
-        vector<vector<int>>res(st.begin(),st.end());
         return res;
+
     }
 };
