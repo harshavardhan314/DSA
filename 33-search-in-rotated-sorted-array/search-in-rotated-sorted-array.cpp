3a54fffc4eec5babbfaf891574dd681b86c5c66a
Harsha_314 <hharshavardhan974@gmail.com>
2026-05-28 22:52:21 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (95.99%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -3,33 +3,27 @@ public:
     int search(vector<int>& arr, int target) {
 
         int n=arr.size();
-        
         int l=0;
         int h=n-1;
-
         while(l<=h){
-
             int mid=(l+h)/2;
 
-            if(arr[mid]==target){
-                return mid;
-            }
-
-            // if my array is increasing from l.. to ..mid
-            // then i will check in that range..
+            if(arr[mid]==target)return mid;
+            // left part sorted
             if(arr[l]<=arr[mid]){
-                if(target>=arr[l] && target<=arr[mid]){
+
+                if(target>= arr[l] && target<=arr[mid]){
                     h=mid-1;
                 }
                 else{
                     l=mid+1;
                 }
-            }
 
-            // if my array is increasing from mid to h
-            // then check in that range...
+            }
             else if(arr[mid]<=arr[h]){
+
                 if(target>=arr[mid] && target<=arr[h]){
+                    
                     l=mid+1;
                 }
                 else{
@@ -37,10 +31,6 @@ public:
                 }
 
             }
-
-            
-
-
         }
         return -1;
         
