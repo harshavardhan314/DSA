96329d2b57da558667eccd0f96e357bfbcce5fd6
Harsha_314 <hharshavardhan974@gmail.com>
2026-05-30 20:40:49 +0530

    Time: 0 ms (100.00%) | Memory: 26 MB (54.95%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -3,21 +3,33 @@ public:
     int trap(vector<int>& nums) {
 
         int n=nums.size();
-        vector<int>prefix(n,0);
-        vector<int>suffix(n,0);
-        prefix[0]=nums[0];
-        for(int i=1;i<n;i++){
-            prefix[i]=max(prefix[i-1],nums[i]);
-        }
-        suffix[n-1]=nums[n-1];
-        for(int i=n-2;i>=0;i--){
-            suffix[i]=max(suffix[i+1],nums[i]);
-        }
         int ans=0;
-        for(int i=0;i<n;i++){
-            int left=prefix[i];
-            int right=suffix[i];
-            ans+=min(left,right)-nums[i];
+        int left=0;
+        int right=n-1;
+        int max_left=0;
+        int max_right=0;
+        while(left<=right){
+
+            if(nums[left]<=nums[right]){
+
+                if(nums[left]>=max_left){
+                    max_left=nums[left];
+                }
+                else{
+                    ans+=max_left-nums[left];
+                }
+                left++;
+            }
+            else{
+
+                if(nums[right]>=max_right){
+                    max_right=nums[right];
+                }
+                else{
+                    ans+=max_right-nums[right];
+                }
+                right--;
+            }
         }
         return ans;
 
