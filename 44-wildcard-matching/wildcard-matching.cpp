f50d7a83e31cceafc716ffc3461b57bdb3fd3161
Harsha_314 <hharshavardhan974@gmail.com>
2026-05-30 23:05:55 +0530

    Time: 51 ms (53.63%) | Memory: 54.4 MB (21.04%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,27 +1,27 @@
 bool rec(int i,int j,string &s,string &p,vector<vector<int>>&dp){
-    if(i>=s.size()){
-        if(j>=p.size())return true;
-        for(int k=j;k<p.size();k++){
+    if(i<0){
+        if(j<0)return true;
+        for(int k=j;k>=0;k--){
             if(p[k]!='*')return false;
         }
         return true;
     }
-    if(j>=p.size() && i<s.size())return false;
+    if(j<0 && i>=0)return false;
 
     if(dp[i][j]!=-1) return dp[i][j];
     
     if(p[j]=='?'){
-        return dp[i][j]=rec(i+1,j+1,s,p,dp);
+        return dp[i][j]=rec(i-1,j-1,s,p,dp);
     }
     else if(p[j]=='*'){
         
-        bool empty_space= rec(i,j+1,s,p,dp);
-        bool single_char=rec(i+1,j+1,s,p,dp);
-        bool more_than_one=rec(i+1,j,s,p,dp);
+        bool empty_space= rec(i,j-1,s,p,dp);
+        bool single_char=rec(i-1,j-1,s,p,dp);
+        bool more_than_one=rec(i-1,j,s,p,dp);
         return dp[i][j]=empty_space or single_char or more_than_one; 
     }
     else if(s[i]==p[j]){
-        return dp[i][j]=rec(i+1,j+1,s,p,dp);
+        return dp[i][j]=rec(i-1,j-1,s,p,dp);
     }
     else{
         return dp[i][j]=false;
@@ -36,7 +36,7 @@ public:
         int m=p.size();
         vector<vector<int>>dp(n,vector<int>(m,-1));
 
-        return rec(0,0,s,p,dp);
+        return rec(n-1,m-1,s,p,dp);
         
     }
 };
