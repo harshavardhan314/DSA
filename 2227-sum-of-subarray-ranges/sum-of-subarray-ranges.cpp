3a782e047c7ea3b4b61f97f4697906c27e1ba624
Harsha_314 <hharshavardhan974@gmail.com>
2026-06-01 01:10:52 +0530

    Time: 11 ms (26.53%) | Memory: 16 MB (29.47%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -1,19 +1,81 @@
+long long find_sum_subarray_minimums(vector<int>&nums){
+    int n=nums.size();
+
+    vector<int>nse(n,n);
+    stack<int>st;
+    for(int i=n-1;i>=0;i--){
+
+        while(!st.empty() && nums[i]<nums[st.top()]){
+            st.pop();
+        }
+        nse[i]=st.empty()?n:st.top();
+        st.push(i);
+    }
+
+
+    vector<int>pse(n,-1);
+    stack<int>st2;
+    for(int i=0;i<n;i++){
+
+        while(!st2.empty() && nums[i]<=nums[st2.top()]){
+            st2.pop();
+        }
+        pse[i]=st2.empty()?-1:st2.top();
+        st2.push(i);
+    }
+
+    long long ans=0;
+    for(int i=0;i<n;i++){
+
+        int left=i-pse[i];
+        int right=nse[i]-i;
+        ans+=1LL*left*right*nums[i];
+    }
+    return ans;
+
+
+}
+
+long long find_sum_subarrays_max(vector<int>&nums){
+
+    int n=nums.size();
+
+    vector<int>nge(n,n);
+
+    stack<int>st;
+    for(int i=n-1;i>=0;i--){
+        while(!st.empty() && nums[i]>nums[st.top()]){
+            st.pop();
+        }
+        nge[i]=st.empty()?n:st.top();
+        st.push(i);
+    }
+
+    stack<int>st2;
+
+    vector<int>pge(n,-1);
+    for(int i=0;i<n;i++){
+        while(!st2.empty() && nums[i]>=nums[st2.top()]){
+            st2.pop();
+        }
+        pge[i]=st2.empty()?-1:st2.top();
+        st2.push(i);
+    }
+    long long ans=0;
+
+    for(int i=0;i<n;i++){
+        int left=i-pge[i];
+        int right=nge[i]-i;
+        ans+=1LL*left*right*nums[i];
+    }
+    return ans;
+}
+
 class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
 
-        long long ans=0;
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            int min_val=nums[i];
-            int max_val=nums[i];
-            for(int j=i;j<n;j++){
-                min_val=min(min_val,nums[j]);
-                max_val=max(max_val,nums[j]);
-                ans+=max_val-min_val;
-            }
-        }
-        return ans;
+        return find_sum_subarrays_max(nums)-find_sum_subarray_minimums(nums);
         
     }
 };
