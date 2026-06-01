01fb310e5327ca318f82a1ee14d5f59828ff92ef
Harsha_314 <hharshavardhan974@gmail.com>
2026-06-01 15:45:23 +0530

    Time: 6 ms (70.28%) | Memory: 18.9 MB (84.77%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -1,67 +1,54 @@
 class Solution {
 public:
-    vector<int>find_nse(vector<int>&heights)
-    {
-        int n=heights.size();
-        vector<int>nse(n,0);
-        stack<int>st;
-        for(int i=n-1;i>=0;i--)
-        {
-            while(!st.empty() && heights[i]<=heights[st.top()])
-            {
+
+    int largestRectangle(vector<int>& h) {
+        stack<int> st;
+        int n = h.size();
+        int ans = 0;
+
+        for (int i = 0; i <= n; i++) {
+
+            int currHeight = (i == n ? 0 : h[i]);
+
+            while (!st.empty() && currHeight <= h[st.top()]) {
+                int height = h[st.top()];
                 st.pop();
+
+                int left = st.empty() ? -1 : st.top();
+                int width = i - left - 1;
+
+                ans = max(ans, height * width);
             }
-            nse[i]=st.empty() ? n:st.top();
+
             st.push(i);
-        }
-        return nse;
-    }
-    vector<int>find_pse(vector<int>&heights)
-    {
-        int n=heights.size();
-        vector<int>pse(n,0);
-        stack<int>s;
-        for(int i=0;i<n;i++)
-        {
-            while(!s.empty() && heights[i]<=heights[s.top()])
-            {
-                s.pop();
-            }
-            pse[i]=s.empty() ? -1:s.top();
-            s.push(i);
-        }
-        return pse;
-    }
-
-    
-    int maximalRectangle(vector<vector<char>>& mat) {
-        int n=mat.size();
-        int m=mat[0].size();
-        vector<int>hist(m,0);
-        int ans=0;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(mat[i][j]=='1')
-                hist[j]++;
-                else
-                hist[j]=0;
-            }
-            vector<int>nse=find_nse(hist);
-            vector<int>pse=find_pse(hist);
-            for(int k=0;k<m;k++)
-            {
-                int area=(nse[k]-pse[k]-1)*hist[k];
-                ans=max(ans,area);
-
-            }
-
-
         }
 
         return ans;
+    }
 
-        
+    int maximalRectangle(vector<vector<char>>& matrix) {
+
+        if (matrix.empty()) return 0;
+
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        vector<int> height(m, 0);
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < m; j++) {
+
+                if (matrix[i][j] == '1')
+                    height[j]++;
+                else
+                    height[j] = 0;
+            }
+
+            ans = max(ans, largestRectangle(height));
+        }
+
+        return ans;
     }
 };
