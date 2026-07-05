3aa85887fa8acb6c1df2c4796061ce1eca27fd80
Harsha_314 <hharshavardhan974@gmail.com>
2026-07-05 23:24:37 +0530

    Time: 23 ms (31.01%) | Memory: 17.8 MB (27.87%) - LeetSync

--- 1234-number-of-paths-with-max-score/number-of-paths-with-max-score.cpp
+++ 1234-number-of-paths-with-max-score/number-of-paths-with-max-score.cpp
@@ -1,71 +1,66 @@
-int find_max_sum(int i,int j,vector<string>&board,vector<vector<vector<int>>>&dp,int sum,int req){
-    long long mod=1e9+7;
-
-    if(board[i][j]=='X'){
-        return 0;
-    }
-
-    if(i==0 && j==0){
-        if(sum==req){
-            return 1;
-        }
-        return 0;
-
-    }
-
-    if(dp[i][j][sum]!=-1){
-        return dp[i][j][sum];
-    }
-    int up=0,left=0,diagonal=0;
-    if(i-1>=0){
-        int val=board[i][j]-'0';
-        up+=find_max_sum(i-1,j,board,dp,sum+val,req);
-    }
-    if(j-1>=0 ){
-        int val=board[i][j]-'0';
-        left+=find_max_sum(i,j-1,board,dp,sum+val,req);
-    }
-    if(i-1>=0 && j-1>=0 ){
-        int val=board[i][j]-'0';
-        diagonal+=find_max_sum(i-1,j-1,board,dp,sum+val,req);
-
-    }
-    return dp[i][j][sum]=(up+left+diagonal)%mod;
-}
-
 class Solution {
 public:
     vector<int> pathsWithMaxScore(vector<string>& board) {
-        int n=board.size();
-        board[n-1][n-1]='0';
-        vector<vector<int>>dp(n,vector<int>(n,INT_MIN));
-        dp[n-1][n-1]=0;
-        for(int i=n-2;i>=0;i--){
-            if(board[n-1][i]!='X'){
-                dp[n-1][i]=dp[n-1][i+1]+(board[n-1][i]-'0');
-            }
-        }
-        for(int i=n-2;i>=0;i--){
-            if(board[i][n-1]!='X'){
-                dp[i][n-1]=dp[i+1][n-1]+(board[i][n-1]-'0');
-            }
-        }
-        for(int i=n-2;i>=0;i--){
-            for(int j=n-2;j>=0;j--){
-                if(i==0 && j==0){
-                    dp[0][0]=max({dp[i+1][j+1],dp[i+1][j],dp[i][j+1]});
-                }
-                else if(board[i][j]!='X'){
-                    dp[i][j]=max({dp[i+1][j+1],dp[i+1][j],dp[i][j+1]})+(board[i][j]-'0');
+        const int MOD = 1e9 + 7;
+        int n = board.size();
+
+        
+        vector<vector<int>> score(n, vector<int>(n, INT_MIN));
+
+        
+        vector<vector<int>> ways(n, vector<int>(n, 0));
+
+        score[n - 1][n - 1] = 0;
+        ways[n - 1][n - 1] = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+
+                if (i == n - 1 && j == n - 1)
+                    continue;
+
+                if (board[i][j] == 'X')
+                    continue;
+
+                int best = INT_MIN;
+                long long cnt = 0;
+
+                vector<pair<int, int>> dir = {
+                    {i + 1, j},
+                    {i, j + 1},
+                    {i + 1, j + 1}
+                };
+
+                for (auto [x, y] : dir) {
+                    if (x >= n || y >= n)
+                        continue;
+
+                    if (score[x][y] == INT_MIN)
+                        continue;
+
+                    if (score[x][y] > best) {
+                        best = score[x][y];
+                        cnt = ways[x][y];
+                    } else if (score[x][y] == best) {
+                        cnt = (cnt + ways[x][y]) % MOD;
+                    }
                 }
 
+                if (best == INT_MIN)
+                    continue;
+
+                int val = 0;
+                if (board[i][j] != 'S' && board[i][j] != 'E')
+                    val = board[i][j] - '0';
+
+                score[i][j] = best + val;
+                ways[i][j] = cnt % MOD;
             }
         }
-       cout<<dp[0][0]<<endl;
-        if(dp[0][0]<0)return {0,0};
-        vector<vector<vector<int>>>dp2(n,vector<vector<int>>(n,vector<int>(2000,-1)));
-        int val=find_max_sum(n-1,n-1,board,dp2,0,dp[0][0]);
-    
-        return {dp[0][0],val};
+
+        if (score[0][0] == INT_MIN)
+            return {0, 0};
+
+        return {score[0][0], ways[0][0]};
     }
 };
