561c5d6694fd8fb920f220dc79fda02dacf5a32d
Harsha_314 <hharshavardhan974@gmail.com>
2026-07-08 18:59:28 +0530

    Time: 184 ms (38.78%) | Memory: 135.6 MB (35.15%) - LeetSync

--- 4174-lexicographically-maximum-mex-array/lexicographically-maximum-mex-array.cpp
+++ 4174-lexicographically-maximum-mex-array/lexicographically-maximum-mex-array.cpp
@@ -2,38 +2,41 @@ class Solution {
 public:
     vector<int> maximumMEX(vector<int>& nums) {
         int n=nums.size();
-
-        vector<int>best_mex(n,0);
-        int mex=0;
-        map<int,int>mp;
+        vector<int>suffix(n,0);
+        set<int>st;
+        int req=0;
         for(int i=n-1;i>=0;i--){
-            mp[nums[i]]++;
-            while(mp.find(mex)!=mp.end()){
-                mex++;
+            st.insert(nums[i]);
+            while(!st.empty() && st.find(req)!=st.end()){
+                st.erase(req);
+                req++;
+                
             }
-            best_mex[i]=mex;
+            suffix[i]=req;
+
         }
         
-        mp.clear();
-        int want=best_mex[0];
-        mex=0;
         vector<int>ans;
+        set<int>have;
+        int want=suffix[0];
+        int curr=0;
         for(int i=0;i<n;i++){
-            mp[nums[i]]++;
-            while(mp.find(mex)!=mp.end()){
-                mex++;
+            have.insert(nums[i]);
+            while(!have.empty() && have.find(curr)!=have.end()){
+                have.erase(curr);
+                curr++;
             }
-            if(mex==want){
-                ans.push_back(mex);
-                mex=0;
-                mp.clear();
-                if(i+1<n)
-                want=best_mex[i+1];
+            if(curr==want){
+                ans.push_back(curr);
+                if(i+1<n){
+                    want=suffix[i+1];
+                    curr=0;
+                    have.clear();
+                }
             }
+
 
         }
         return ans;
-
-        
     }
 };
