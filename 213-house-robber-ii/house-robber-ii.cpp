9c0a49c12ca8ab5fa3bfff1ef085b1234a218018
Harsha_314 <hharshavardhan974@gmail.com>
2026-07-12 22:28:34 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (37.10%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
-    int rec(int i,vector<int>&nums,vector<int>&dp)
-    {
-        int n=nums.size();
-        if(i>=n)
-        return 0;
-        if(dp[i]!=-1)return dp[i];
-
-        int pick=nums[i]+rec(i+2,nums,dp);
-        int not_pick=rec(i+1,nums,dp);
-        return dp[i]=max(pick,not_pick);
-    }
     int rob(vector<int>& nums) {
+
+
         int n=nums.size();
         if(n==1)return nums[0];
-        vector<int>dp(n,-1);
-        int last=nums.back();
-        nums.pop_back();
-        int ans1= rec(0,nums,dp);
-        for(int i=0;i<n;i++)dp[i]=-1;
-        nums.push_back(last);
-        int ans2=rec(1,nums,dp);
+        vector<int>dp(n,0);
+        dp[0]=nums[0];
+        for(int i=1;i<n-1;i++){
+            dp[i]=nums[i];
+            if(i-2>=0){
+                dp[i]+=dp[i-2];
+            }
+            dp[i]=max(dp[i-1],dp[i]);
+        }
+        int val=dp[n-2];
+        dp.assign(n,0);
+        for(int i=1;i<n;i++){
+            dp[i]=nums[i];
+            if(i-2>=0){
+                dp[i]+=dp[i-2];
+            }
+            dp[i]=max(dp[i-1],dp[i]);
+        }
+        return max(dp[n-1],val);
 
-        return max(ans1,ans2);
         
     }
 };
