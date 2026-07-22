2e583e1436370be1df7d219253ff9333b2e96ef7
Harsha_314 <hharshavardhan974@gmail.com>
2026-07-22 11:09:26 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (5.91%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -5,19 +5,15 @@ public:
         int i=m-1;
         int j=n-1;
         int k=n+m-1;
-        while(j>=0){
-
-            if(i>=0 && nums1[i]>nums2[j]){
-                
-                    nums1[k]=nums1[i];
-                    i--;
-                    k--;
-                
+        for(int k=n+m-1;k>=0;k--){
+            if(i>=0 && j>=0 && nums1[i]>nums2[j]){
+                nums1[k]=nums1[i];
+                i--;
             }
-            else{
+            else if(j>=0){
                 nums1[k]=nums2[j];
                 j--;
-                k--;
+
             }
         }
         
