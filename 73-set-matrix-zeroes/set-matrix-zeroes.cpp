a214f9978988d7bf8d78647d4905835eb571f62b
Harsha_314 <hharshavardhan974@gmail.com>
2026-07-22 09:20:34 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (94.22%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -3,19 +3,38 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n=matrix.size();
         int m=matrix[0].size();
-        vector<pair<int,int>>vp;
+
+        int col0=1;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(matrix[i][j]==0)
-                vp.push_back({i,j});
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    if(j!=0)matrix[0][j]=0;
+                    else col0=0;
+                }
             }
         }
-        for(auto [r,c]:vp){
-            for(int i=0;i<m;i++){
-                matrix[r][i]=0;
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
+                
             }
+        }
+
+        // now we make intial row as zero because if we make first col as zero 
+        // then first row can be overwritten
+
+        if(matrix[0][0]==0){
+            for(int i=0;i<m;i++){
+                matrix[0][i]=0;
+            }
+        }
+        if(col0==0){
             for(int i=0;i<n;i++){
-                matrix[i][c]=0;
+                matrix[i][0]=0;
             }
         }
         
