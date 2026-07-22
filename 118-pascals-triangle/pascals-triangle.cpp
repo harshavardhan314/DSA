c73cfe90bc11b428a49b458f65f0e614d9abcb3b
Harsha_314 <hharshavardhan974@gmail.com>
2026-07-22 09:27:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (63.83%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -2,27 +2,27 @@ class Solution {
 public:
     vector<vector<int>> generate(int n) {
 
-        if(n==1){
-            return {{1}};
-        }
         vector<vector<int>>res;
-        res.push_back({1});
-
-        for(int i=1;i<n;i++){
-            vector<int>curr_row;
-            curr_row.push_back(1);
-
-            for(int j=0;j<res[i-1].size();j++){
-                if(j+1<res[i-1].size()){
-                    curr_row.push_back(res[i-1][j] + res[i-1][j+1]);
-                }
-            }
-            curr_row.push_back(1);
-
-            res.push_back(curr_row);
+        for(int i=0;i<n;i++){
+            vector<int>temp(i+1,1);
+            res.push_back(temp);
         }
-
+        // for(auto it:res){
+        //     for(auto i:it){
+        //         cout<<i<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        
+        for(int i=2;i<n;i++){
+            for(int j=1;j<res[i].size()-1;j++){
+                res[i][j]=res[i-1][j-1]+res[i-1][j];
+            }
+        }
         return res;
+
+        // T.c=o(n*m)
+        // s.c=O(N*M)
         
     }
 };
