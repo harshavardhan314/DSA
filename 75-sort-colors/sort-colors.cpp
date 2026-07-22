e5e983bcacf999e5b44112d38a2e16db15eb397a
Harsha_314 <hharshavardhan974@gmail.com>
2026-07-22 09:56:21 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (49.36%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,40 +1,24 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-
         int n=nums.size();
-
         int low=0;
-        int high=n-1;
         int mid=0;
-
-        // [0...low-1] ----> all zeros..
-        // [low.. mid-1] ----> all ones..
-        // [mid...high]---> unsorted array
-        // [high+1...n-1]----> all twos..
-
-        // we try to make this unsorted array to sort..
-
+        int high=n-1;
         while(mid<=high){
-
             if(nums[mid]==0){
-                swap(nums[mid],nums[low]);
-                low++;
+                swap(nums[low],nums[mid]);
                 mid++;
+                low++;
             }
             else if(nums[mid]==1){
                 mid++;
             }
             else{
-                swap(nums[mid],nums[high]);
+                swap(nums[high],nums[mid]);
                 high--;
-
             }
         }
-
-        // T.c= O(n)
-        // s.c=O(1)
-
-        
+     
     }
 };
