10dfb1c67cae4a4b189774de72c9ee4f9fa902da
Harsha_314 <hharshavardhan974@gmail.com>
2026-07-24 09:59:37 +0530

    Time: 305 ms (28.53%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,6 +1,6 @@
-SELECT (
-    SELECT DISTINCT salary
-    FROM Employee
-    ORDER BY salary DESC
-    LIMIT 1 OFFSET 1
-) AS SecondHighestSalary;
+select max(salary) as SecondHighestSalary
+from employee
+where salary <> (
+    select max(salary)
+    from employee
+)
