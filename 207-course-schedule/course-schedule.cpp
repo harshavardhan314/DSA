754ebf05ac288ca4fca345559ca34441c07f5002
Harsha_314 <hharshavardhan974@gmail.com>
2026-08-29 18:41:14 +0530

    Time: 5 ms (55.56%) | Memory: 19.4 MB (55.83%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -2,31 +2,34 @@ class Solution {
 public:
     bool canFinish(int n, vector<vector<int>>& p) {
         
-        vector<int>indeg(n,0);
-
+        vector<int>indegree(n,0);
         vector<vector<int>>adj(n);
-        for(auto it:p){
-            adj[it[1]].push_back(it[0]);
-            indeg[it[0]]++;
+        for(int i=0;i<p.size();i++){
+            int u=p[i][0];
+            int v=p[i][1];
+            adj[v].push_back(u);
+            indegree[u]++;
         }
         queue<int>q;
         for(int i=0;i<n;i++){
-            if(indeg[i]==0)
-            q.push(i);
-        
+            if(indegree[i]==0)q.push(i);
         }
         if(q.empty())return false;
-        
         vector<int>order;
         while(!q.empty()){
-            int node=q.front();
+            auto node=q.front();
             order.push_back(node);
             q.pop();
             for(auto it:adj[node]){
-                indeg[it]--;
-                if(indeg[it]==0)q.push(it);
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
             }
+
         }
         return n==order.size();
+
+
     }
 };
