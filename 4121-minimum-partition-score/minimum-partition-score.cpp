50a672a461b207728a18b644ac0ce7c0e30c8b9b
Harsha_314 <hharshavardhan974@gmail.com>
2026-08-29 11:05:28 +0530

    Time: 951 ms (36.21%) | Memory: 110.6 MB (25.00%) - LeetSync

--- 4121-minimum-partition-score/minimum-partition-score.cpp
+++ 4121-minimum-partition-score/minimum-partition-score.cpp
@@ -1,54 +1,44 @@
-class Solution {
-    long long pre[1001];
-    long long dp[1001][1001]; 
-    int n;
+long long solve(int i,vector<int>&nums,int k,vector<long long int>&p,vector<vector<long long >>&dp){
 
-    long long solve(int i, int par, vector<int>& nums) {
-        
-        if (par == 0) {
-            long long last_seg = pre[n] - pre[i];
-            return (last_seg * (last_seg + 1)) / 2;
-        }
+    int n=nums.size();
+    
 
-        
-        if (dp[i][par] != -1) return dp[i][par];
-
-        long long ans = 1e18; 
-
-        
-        for (int j = i; j <= n - 1 - par; j++) {
-            long long sum = pre[j + 1] - pre[i];
-            long long cost = (sum * (sum + 1)) / 2;
-
-            if(cost>=ans)break;
-
-            
-            ans = min(ans,cost+ solve(j + 1, par - 1, nums));
-
-        
-            
-            
-        }
-        return dp[i][par] = ans;
+     
+    if(k==0){
+        long long temp=p[n-1];
+        if(i-1>=0)temp-=p[i-1];
+        return (temp*(temp+1))/2;
     }
+      if(dp[i][k]!=-1) return dp[i][k]; 
+      
+    long long ans=LONG_MAX;
+    for(int j=i;j<n-k;j++){
+        long long int sum=p[j];
+        if(i-1>=0)sum-=p[i-1];
+        long long int curr_val=(sum*(sum+1))/2;
+        if(curr_val>=ans)break;
 
+        ans=min(ans,curr_val+solve(j+1,nums,k-1,p,dp));
+    }
+    return dp[i][k]=ans;
+}
+
+
+class Solution {
 public:
     long long minPartitionScore(vector<int>& nums, int k) {
-        n = nums.size();
-        
-        
-        pre[0] = 0;
-        for (int i = 0; i < n; i++) {
-            pre[i + 1] = pre[i] + nums[i];
+
+        int n=nums.size();
+        vector<long long int>p(n,0);
+        for(int i=0;i<n;i++){
+            p[i]=nums[i];
+            if(i-1>=0)p[i]+=p[i-1];
         }
 
-        
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= k; j++) {
-                dp[i][j] = -1;
-            }
-        }
+        vector<vector<long long >>dp(n,vector<long long >(k+1,-1));
 
-        return solve(0, k - 1, nums);
+        return solve(0,nums,k-1,p,dp);
+
+        
     }
 };
